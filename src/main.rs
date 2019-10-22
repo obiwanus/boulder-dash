35c4de3d540ef6256a88af96401fdb9ae22b7e1c
Ivan Ivanov <ivan@ivanovs.info>
2019-10-22 15:50:25 +0200

    Display a black window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use amethyst::{
-    core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resource, SystemData},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -16,6 +14,24 @@ impl SimpleState for Pong {}
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
+
+    game.run();
 
     Ok(())
 }
