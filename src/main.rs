f2fe994022f55ee492d0d888fa28177c451feae6
Ivan Ivanov <ivan@ivanovs.info>
2019-12-07 08:14:05 +0100

    Fill window with solid color using opengl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate gl;
 extern crate sdl2;
 
 fn main() {
@@ -5,17 +6,28 @@ fn main() {
     let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
         .window("Boulder Dash", 1024, 768)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+
+    let gl_context = window.gl_create_context().unwrap();
+    let gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => break 'main,
-                _ => {},
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
             }
+
+            unsafe {
+                gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+            }
+            window.gl_swap_window();
         }
     }
 }
