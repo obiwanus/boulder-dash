a26e723aa9fba740c14b9f122fc5bf8488577967
Ivan Ivanov <ivan@ivanovs.info>
2019-12-07 18:44:51 +0100

    Halfway through to shader implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 extern crate gl;
 extern crate sdl2;
 
+mod shader;
+use shader::Program;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
     let window = video_subsystem
         .window("Boulder Dash", 1024, 768)
         .opengl()
@@ -11,9 +19,20 @@ fn main() {
         .build()
         .unwrap();
 
-    let gl_context = window.gl_create_context().unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
     let gl =
         gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    unsafe {
+        gl::Viewport(0, 0, 1024, 768);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let shader_program = Program::new()
+        .vertex_shader("shaders/triangle.vert")
+        .fragment_shader("shaders/triangle.frag")
+        .link();
+    shader_program.set_used();
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -24,7 +43,6 @@ fn main() {
             }
 
             unsafe {
-                gl::ClearColor(0.3, 0.3, 0.5, 1.0);
                 gl::Clear(gl::COLOR_BUFFER_BIT);
             }
             window.gl_swap_window();
---
+++ src/shader.rs
@@ -0,0 +1,57 @@
+use gl;
+use gl::types::{GLchar, GLenum, GLint, GLuint};
+use std::error::Error;
+use std::ffi::CString;
+use std::fs;
+
+pub struct Program {
+    id: GLuint,
+}
+
+impl Program {
+    pub fn new() -> Program {
+        let program_id = unsafe { gl::CreateProgram() };
+        Program { id: program_id }
+    }
+
+    pub fn vertex_shader(self, path: &str) -> Self {
+        self
+    }
+}
+
+struct Shader {
+    id: GLuint,
+}
+
+impl Shader {
+    pub fn new(kind: GLenum, path: &str) -> Result<Self, Box<dyn Error>> {
+        let source = CString::new(fs::read_to_string(path)?)?;
+        let id = unsafe { gl::CreateShader(kind) };
+        unsafe {
+            gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+            gl::CompileShader(id);
+        }
+        let mut success: GLint = 1;
+        unsafe {
+            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+        }
+        if success == 0 {
+            let mut len: GLint = 0;
+            unsafe {
+                gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+            let error = new_cstring(len as usize);
+            unsafe {
+                gl::GetShaderInfoLog(id, len, std::ptr::null_mut(), error.as_ptr() as *mut GLchar);
+            }
+            return Err(Box::new(error.to_string_lossy().into_owned()));
+        }
+
+        Ok(Shader { id })
+    }
+}
+
+fn new_cstring(len: usize) -> CString {
+    let buffer: Vec<u8> = vec![0; len];
+    unsafe { CString::from_vec_unchecked(buffer) }
+}
