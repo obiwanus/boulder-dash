07f138348b1dabdcf4d788062bcd4c0fe96aeb7e
Ivan Ivanov <ivan@ivanovs.info>
2019-12-07 07:42:02 +0100

    Create a window on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate sdl2;
+
 fn main() {
-    println!("Hey");
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Boulder Dash", 1024, 768)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+    }
 }
