3add6b21b539a17d95b296add1d1e821c03c5ab5
Ivan Ivanov <ivan@ivanovs.info>
2019-12-20 10:22:50 +0100

    Move the light around

--- assets/shaders/cube/cube.frag
+++ assets/shaders/cube/cube.frag
@@ -15,20 +15,20 @@ uniform vec3 light_pos;
 uniform vec3 camera_pos;
 
 void main() {
+    const float MAX_DISTANCE = 20.0;
     vec3 normal = normalize(IN.normal);
+    vec3 light_direction = normalize(light_pos - IN.frag_pos);
+    float light_distance = length(light_pos - IN.frag_pos);
+    float distance_effect = min(mix(1.0, 0.1, light_distance / MAX_DISTANCE), 1.0);
+
+    // Diffuse
+    const float diffuse_strength = 0.3;
+    float diffuse_impact = max(dot(normal, light_direction), 0.0);
+    vec3 diffuse = diffuse_strength * diffuse_impact * light_color;
 
     // Ambient
     float ambient_strength = 0.1;
     vec3 ambient = ambient_strength * light_color;
-
-    // Diffuse
-    const float MAX_DISTANCE = 7.0;
-    vec3 light_direction = normalize(light_pos - IN.frag_pos);
-    float light_distance = length(light_pos - IN.frag_pos);
-    float diffuse_impact =
-        max(dot(normal, light_direction), 0.0) *
-        max((1 - light_distance / MAX_DISTANCE), 0.0);
-    vec3 diffuse = diffuse_impact * light_color;
 
     // Specular
     float specular_strength = 0.5;
@@ -37,6 +37,6 @@ void main() {
     float spec = pow(max(dot(view_direction, reflection), 0.0), 32);
     vec3 specular = specular_strength * spec * light_color;
 
-    Color = vec4(ambient + diffuse + specular, 1.0) *
+    Color = vec4(ambient + (diffuse + specular) * distance_effect, 1.0) *
             mix(texture(wall, IN.tex_coord), texture(face, IN.tex_coord - 0.5), 0.2);
 }
--- src/main.rs
+++ src/main.rs
@@ -126,9 +126,7 @@ fn run() -> Result<(), failure::Error> {
         glm::vec3(-1.3, 1.0, -1.5),
     ];
 
-    let light_position = glm::vec3(1.2, 1.0, 2.0);
-    let light_model = glm::translation(&light_position);
-    let light_model = glm::scale(&light_model, &glm::vec3(0.2, 0.2, 0.2));
+    let mut light_position = glm::vec3(1.2, 1.0, 2.0);
 
     // Buffers
     let stride = 8;
@@ -159,15 +157,12 @@ fn run() -> Result<(), failure::Error> {
     cube_shader.set_texture_unit("wall", 0)?;
     cube_shader.set_texture_unit("face", 1)?;
     cube_shader.set_vec3("light_color", glm::vec3(1.0, 1.0, 1.0))?;
-    cube_shader.set_vec3("light_pos", light_position)?;
 
     // Light shader
     let light_shader = Program::new()
         .vertex_shader("assets/shaders/light/light.vert")?
         .fragment_shader("assets/shaders/light/light.frag")?
         .link()?;
-    light_shader.set_used();
-    light_shader.set_mat4("model", &light_model)?;
 
     let mut camera = Camera::new();
     camera.aspect_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
@@ -214,22 +209,41 @@ fn run() -> Result<(), failure::Error> {
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
         }
 
+        // Time for rotations etc
+        let seconds_elapsed = SystemTime::now()
+            .duration_since(start_timestamp)
+            .unwrap()
+            .as_secs_f32();
+
         // Transformations
         let proj = camera.get_projection_matrix();
         let view = camera.get_view_matrix();
+
+        // Light cube
+        let x_max = 2.0;
+        let z_max = 5.0;
+        light_position.x = x_max * seconds_elapsed.sin();
+        light_position.z = z_max * seconds_elapsed.cos() - 4.0;
+        let light_model = glm::translation(&light_position);
+        let light_model = glm::scale(&light_model, &glm::vec3(0.1, 0.1, 0.1));
+
+        // Draw light cube
+        light_shader.set_used();
+        light_shader.set_mat4("proj", &proj)?;
+        light_shader.set_mat4("view", &view)?;
+        light_shader.set_mat4("model", &light_model)?;
+        cube.draw_triangles(&light_vao);
 
         // Draw rotating cubes
         cube_shader.set_used();
         cube_shader.set_mat4("proj", &proj)?;
         cube_shader.set_mat4("view", &view)?;
         cube_shader.set_vec3("camera_pos", camera.position)?;
+        cube_shader.set_vec3("light_pos", light_position)?;
 
         wall_texture.bind(0);
         face_texture.bind(1);
-        let seconds_elapsed = SystemTime::now()
-            .duration_since(start_timestamp)
-            .unwrap()
-            .as_secs_f32();
+
         let angle = seconds_elapsed * PI / 5.0;
         for pos in cube_positions.iter() {
             let cube_model = glm::translate(&cube_model, pos);
@@ -239,19 +253,16 @@ fn run() -> Result<(), failure::Error> {
             cube.draw_triangles(&cube_vao);
         }
 
-        // Draw light cube
-        light_shader.set_used();
-        light_shader.set_mat4("proj", &proj)?;
-        light_shader.set_mat4("view", &view)?;
-        cube.draw_triangles(&light_vao);
-
-        // // Rendering time
-        // let render_ms = SystemTime::now()
-        //     .duration_since(frame_start)
-        //     .unwrap()
-        //     .as_micros() as f32
-        //     / 1000.0;
-        // println!("rendering time: {} ms", render_ms);
+        #[cfg(feature = "debug")]
+        {
+            // Display rendering time
+            let render_ms = SystemTime::now()
+                .duration_since(frame_start)
+                .unwrap()
+                .as_micros() as f32
+                / 1000.0;
+            println!("rendering time: {} ms", render_ms);
+        }
 
         window.gl_swap_window();
     }
